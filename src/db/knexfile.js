1d49afb1a2fda98eab6584068719152abd4769af
Malik <malikoabdulaziz@gmail.com>
2021-09-17 11:38:52 +0100

    remove authroute

--- src/db/knexfile.js
+++ src/db/knexfile.js
@@ -3,40 +3,21 @@ require("dotenv").config();
 const { DEV_USER, DEV_PASSWORD } = process.env;
 const { knexSnakeCaseMappers } = require("objection");
 module.exports = {
-  development: {
-    client: "postgresql",
-    connection: {
-      database: "gratis_db",
-      user: "malik",
-      password: 1234,
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
-    seeds: {
-      directory: "seeds/",
-    },
-    ...knexSnakeCaseMappers,
+  client: "postgresql",
+  connection: {
+    database: "gratis_db",
+    user: "malik",
+    password: 1234,
   },
-
-  production: {
-    client: "postgresql",
-    connection: {
-      database: "gratis_db",
-      user: "username",
-      password: "password",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
-    ...knexSnakeCaseMappers,
+  pool: {
+    min: 2,
+    max: 10,
   },
+  migrations: {
+    tableName: "knex_migrations",
+  },
+  seeds: {
+    directory: "seeds/",
+  },
+  ...knexSnakeCaseMappers,
 };
