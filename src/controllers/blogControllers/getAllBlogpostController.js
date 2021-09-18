01fc261068467fbdb18d96dc03cd0a9a74fe8610
Malik <malikoabdulaziz@gmail.com>
2021-09-19 00:01:43 +0100

    add pagination to get blogs route

--- src/controllers/blogControllers/getAllBlogpostController.js
+++ src/controllers/blogControllers/getAllBlogpostController.js
@@ -2,10 +2,20 @@ const db = require("../../db");
 const respond = require("../../helpers/respond");
 
 module.exports = async (req, res) => {
-  // fetch all blogs from db
   if (req.query.page && req.query.limit) {
-    //implement pagination
-    return;
+    let { page, limit } = req.query;
+    page = parseInt(page - 1) * 10;
+    limit = parseInt(limit);
+    try {
+      const result = await db("blogposts").offset(page).limit(limit);
+      if (!result.length) {
+        respond(res, "out of range, not enough records", 400);
+      }
+      respond(res, result, 200);
+      return;
+    } catch (error) {
+      respond(res, "an error occured", 500);
+    }
   }
   try {
     const result = await db("blogposts");
